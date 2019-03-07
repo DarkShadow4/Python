54c911bfe9a7461236726f427737ebfa5a6b6e71
Eric Ayllon <exxericxx@gmail.com>
2019-03-07 16:30:10 +0100

    Corregidos errores de la salida 2 y resuelto el problema de ambigúedad

--- Examen python 1º Bach/habito_consumo_bebidas.py
+++ Examen python 1º Bach/habito_consumo_bebidas.py
@@ -1,30 +1,32 @@
 def get_bebida_alcoholica_mas_consumida(bebidas, alcohol):
-    bebidas1 = bebidas
-    bebidas1.sort(reverse = True)
+    tipos = [1, 2, 3, 4, 5, 6, 7, 8]
+    assigned = list(sorted(zip(tipos, bebidas, alcohol), key=lambda x:x[1], reverse=True))
     max = False
-    print bebidas.index(bebidas1[0])
-    # for i in bebidas1:
-    #     print bebidas.index(i)
-    #     if (bebidas.index(i)+1 in alcohol) and max == False:
-    #         # print bebidas.index(i)
-    #         pass
+    for i in assigned:
+        if i[2] and max == False:
+            max = True
+            return i[0]
 
-bebidas = [0, 0, 0, 0, 0, 0, 0, 0]
-for n_pueblo in range(5):
-    data = ""
-    while data != "FIN":
-        data = raw_input()
-        if data != "FIN":
-            tipo_bebida, litros = data.split(" ")
-            tipo_bebida = int(tipo_bebida)
-            litros = int(litros)
-            bebidas[tipo_bebida-1] += litros
-alcohol = []
-data = ""
-while data != "FIN":
-    data = raw_input()
-    if data not in alcohol:
-        alcohol.append(data)
+bebidas = [0, 0, 0, 0, 0, 0, 0, 10]
+# for n_pueblo in range(1):
+#     data = ""
+#     print "Pueblo", n_pueblo+1
+#     while data != "FIN":
+#         data = raw_input()
+#         if data != "FIN":
+#             tipo_bebida, litros = data.split(" ")
+#             tipo_bebida = int(tipo_bebida)
+#             litros = int(litros)
+#             bebidas[tipo_bebida-1] += litros
+
+
+alcohol = [True, True, True, True, True, True, True, True]
+# data = ""
+# while data != "FIN":
+#     data = raw_input()
+#     if data != "FIN":
+#         if alcohol[data] not True:
+#             alcohol.append(int(data)-1)
+
 print "El tipo de bebida mas bebida es la:", bebidas.index(max(bebidas))+1
-
 print "El tipo de bebida con alcohol mas consumida es la:", get_bebida_alcoholica_mas_consumida(bebidas, alcohol)
