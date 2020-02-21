ad716f522e8cde101a0f2eaa90c4da555808ddca
Eric Ayllon <exxericxx@gmail.com>
2020-02-21 17:17:25 +0100

    se puede

--- Python 3/Google hash code/GoogleBooks/get_points.py
+++ Python 3/Google hash code/GoogleBooks/get_points.py
@@ -12,18 +12,18 @@ def get_input(filename):
         for ID in range(L):
             N, T, M = indata.readline().split()
             N, T, M = int(N), int(T), int(M)
-            booksinlibrary = sorted([int(book) for book in indata.readline().split()], key=lambda book:books[book], reverse=True)
+            booksinlibrary = indata.readline()
             # libraries.append(Library(ID, N, T, M, booksinlibrary, statistics.mean([books[book] for book in booksinlibrary])))
     return(B, L, D, books, libraries)
 
 def get_solution(solution_name):
 
-    with open(filename, "r") as sol:
+    with open(solution_name, "r") as sol:
         libnum = int(sol.readline())
         books = []
         for i in range(libnum):
             library = sol.readline()
-            books = books+[int(book) for book in sol.readline()]
+            books = books+[int(book) for book in sol.readline().split()]
     return (books)
 
 filename = sys.argv[1]
@@ -34,3 +34,5 @@ scanned_books = get_solution(solution_name)
 points = 0
 for book in scanned_books:
     points += books[book]
+
+print (points)
