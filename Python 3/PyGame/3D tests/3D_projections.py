336b47b77a5670612172d84c077a14de5a24ed88
Eric Ayllon <exxericxx@gmail.com>
2020-03-02 21:51:16 +0100

    yay, it worksgit add *./atom.sh  Now lets move to the simulation itself ^^

---
+++ Python 3/PyGame/3D tests/3D_projections.py
@@ -0,0 +1,99 @@
+import pygame, sys, structures_3D
+
+class Projection(object):
+    def __init__(self, width, height):
+        super(Projection, self).__init__()
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Object display")
+        self.background = (255, 255, 255)
+        self.objects = {}
+        self.displayNodes = True
+        self.displayEdges = True
+        self.nodeColour = (0,0,0)
+        self.edgeColour = (125,125,125)
+        self.nodeRadius = 4
+
+    def display(self):
+        """ Draw the objects on the screen. """
+        self.screen.fill(self.background)
+        for thing in self.objects.values():
+            if self.displayEdges:
+                for edge in thing.edges:
+                    pygame.draw.aaline(self.screen, self.edgeColour, (edge.start.x, edge.start.y), (edge.end.x, edge.end.y), 1)
+            if self.displayNodes:
+                for node in thing.nodes:
+                    pygame.draw.circle(self.screen, self.nodeColour, (int(node.x), int(node.y)), self.nodeRadius, 0)
+
+    def run(self):
+        """Create a pygame screen until it is closed."""
+        key_to_function = {
+            pygame.K_LEFT:   (lambda x: x.rotateAll('Y', -0.1)),
+            pygame.K_RIGHT:   (lambda x: x.rotateAll('Y', 0.1)),
+            pygame.K_UP:   (lambda x: x.rotateAll('X', -0.1)),
+            pygame.K_DOWN:   (lambda x: x.rotateAll('X', 0.1)),
+            pygame.K_q:   (lambda x: x.rotateAll('Z', -0.1)),
+            pygame.K_e:   (lambda x: x.rotateAll('Z', 0.1)),
+            pygame.K_d:  (lambda x: x.move_all('x',  10)),
+            pygame.K_a:   (lambda x: x.move_all('x', -10)),
+            pygame.K_s:   (lambda x: x.move_all('y',  10)),
+            pygame.K_w:     (lambda x: x.move_all('y', -10)),
+            pygame.K_LSHIFT: (lambda x: x.scaleAll(1.25)),
+            pygame.K_LCTRL:  (lambda x: x.scaleAll( 0.8))
+        }
+        done = False
+        while not done:
+            keys = pygame.key.get_pressed()
+            for key in key_to_function.keys():
+                if keys[key]:
+                    key_to_function[key](self)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        done = True
+                    # if event.key in key_to_function.keys():
+                    #     key_to_function[event.key](self)
+
+            pygame.time.delay(100)
+            self.display()
+            pygame.display.flip()
+        return done
+
+    def add_object(self, name, thing):
+        self.objects[name] = thing
+
+    def move_all(self, axis, d):
+        if axis in ["x", "y", "x"]:
+            for thing in self.objects.values():
+                thing.move(axis, d)
+    def scaleAll(self, scale):
+        """ Scale all objects by a given scale, centred on the centre of the screen. """
+        centre_x = self.width/2
+        centre_y = self.height/2
+        for thing in self.objects.values():
+            thing.scale((centre_x, centre_y), scale)
+
+    def rotateAll(self, axis, theta):
+        """ Rotate all thing about their centre, along a given axis by a given angle. """
+        rotateFunction = 'rotate' + axis
+        for thing in self.objects.values():
+            centre = thing.get_centre()
+            getattr(thing, rotateFunction)(centre, theta)
+
+# cube
+cube_nodes = [structures_3D.Node(x,y,z) for x in (50,250) for y in (50,250) for z in (50,250)]
+cube_edges = [(n,n+4) for n in range(0,4)] + [(n,n+1) for n in range(0,8,2)] + [(n,n+2) for n in (0,1,4,5)]
+cube_edges = [structures_3D.Edge(cube_nodes[start],cube_nodes[end]) for start, end in cube_edges]
+cube2 = structures_3D.Object(cube_nodes, cube_edges)
+
+
+# cube = structures_3D.Object([], [])
+# cube.add_nodes([(x ,y, z) for x in (50, 250) for y in (50, 250) for z in (50, 250)])
+
+
+# cube2.Show_components()
+pv = Projection(1000, 750)
+pv.add_object("cubo", cube2)
+pv.run()
---
+++ Python 3/PyGame/3D tests/structures_3D.py
@@ -0,0 +1,131 @@
+import pygame, math
+
+class Node(object):
+    def __init__(self, x, y, z):
+        super(Node, self).__init__()
+        self.x = x
+        self.y = y
+        self.z = z
+
+class Edge(object):
+    def __init__(self, node1, node2):
+        super(Edge, self).__init__()
+        self.start = node1
+        self.end = node2
+
+class Object(object):
+    def __init__(self, nodes=[], edges=[]):
+        self.nodes = nodes
+        self.edges = edges
+
+    def add_nodes(self, nodes):
+        for node in nodes:
+            if node not in self.nodes:
+                self.nodes.append(node)
+    def add_edges(self, edges):
+        for edge in edges:
+            if edge not in self.edges:
+                self.edges.append(edge)
+    def remove_node(self, node):
+        if node in self.nodes:
+            del self.nodes[self.nodes.index(node)]
+    def remove_edge(self, edge):
+        if edge in self.edges:
+            self.edges[self.edges.index(edge)]
+    def Show_components(self):
+        if len(self.nodes) > 0:
+            for i, node in enumerate(self.nodes):
+                print("Node {0} in ({1}, {2}, {3})".format(i, node.x, node.y, node.z))
+        else:
+            print("No nodes in this object")
+        if len(self.edges) > 0:
+            for i, edge in enumerate(self.edges):
+                print("Edge {0} goes from ({1}, {2}, {3}) to ({4}, {5}, {6})".format(i, edge.start.x, edge.start.y, edge.start.z, edge.end.x, edge.end.y, edge.end.z))
+        else:
+            print("No edges in this object")
+    def move(self, axis, d):
+        if axis in ["x", "y", "z"]:
+            for node in self.nodes:
+                setattr(node, axis, getattr(node, axis) + d)
+    def scale(self, centre, scale):
+        """ Scale the wireframe from the centre of the screen. """
+        centre_x, centre_y = centre
+        for node in self.nodes:
+            node.x = centre_x + scale * (node.x - centre_x)
+            node.y = centre_y + scale * (node.y - centre_y)
+            node.z *= scale
+
+    def get_centre(self):
+        """Returns the centre of the object"""
+        n_nodes = len(self.nodes)
+        centre_x = sum(node.x for node in self.nodes)/n_nodes
+        centre_y = sum(node.y for node in self.nodes)/n_nodes
+        centre_z = sum(node.z for node in self.nodes)/n_nodes
+        return(centre_x, centre_y, centre_z)
+
+    def rotateX(self, centre, radians):
+        centre_x, centre_y, centre_z = centre
+        for node in self.nodes:
+            y = node.y-centre_y
+            z = node.z-centre_z
+            d = math.hypot(y, z)
+            theta = math.atan2(y, z) + radians
+            node.y = centre_y + d*math.sin(theta)
+            node.z = centre_z + d*math.cos(theta)
+
+    def rotateY(self, centre, radians):
+        centre_x, centre_y, centre_z = centre
+        for node in self.nodes:
+            x = node.x-centre_x
+            z = node.z-centre_z
+            d = math.hypot(x, z)
+            theta = math.atan2(x, z) + radians
+            node.x = centre_x + d*math.sin(theta)
+            node.z = centre_z + d*math.cos(theta)
+
+    def rotateZ(self, centre, radians):
+        centre_x, centre_y, centre_z = centre
+        for node in self.nodes:
+            x = node.x-centre_x
+            y = node.y-centre_y
+            d = math.hypot(y, x)
+            theta = math.atan2(y, x) + radians
+            node.x = centre_x + d*math.cos(theta)
+            node.y = centre_y + d*math.sin(theta)
+
+# nodes = [
+#     Node(10, 10, 10),
+#     Node(20, 10, 10),
+#     Node(10, 20, 10),
+#     Node(20, 20, 10),
+#     Node(10, 10, 20),
+#     Node(20, 10, 20),
+#     Node(10, 20, 20),
+#     Node(20, 20, 20)
+# ]
+#
+# edges = [
+#     Edge(nodes[0], nodes[1]),
+#     Edge(nodes[0], nodes[2]),
+#     Edge(nodes[3], nodes[1]),
+#     Edge(nodes[3], nodes[2]),
+#
+#     Edge(nodes[4], nodes[5]),
+#     Edge(nodes[4], nodes[6]),
+#     Edge(nodes[7], nodes[5]),
+#     Edge(nodes[7], nodes[6]),
+#
+#     Edge(nodes[4], nodes[0]),
+#     Edge(nodes[5], nodes[1]),
+#     Edge(nodes[6], nodes[2]),
+#     Edge(nodes[7], nodes[3])
+#
+# ]
+#
+# cube = Object(nodes, edges)
+
+#cube
+# cube_nodes = [(x,y,z) for x in (0,1) for y in (0,1) for z in (0,1)]
+# cube_edges = [(n,n+4) for n in range(0,4)] + [(n,n+1) for n in range(0,8,2)] + [(n,n+2) for n in (0,1,4,5)]
+
+# cube2 = Object(cube_nodes, cube_edges)
