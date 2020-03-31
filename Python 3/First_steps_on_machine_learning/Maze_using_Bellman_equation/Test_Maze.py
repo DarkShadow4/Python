4643747505b4f2e1677be835c9cccdc1e5fe4fb8
Eric Ayllon <exxericxx@gmail.com>
2020-03-31 13:58:19 +0200

    maze done, still have to do the ai to solve it with the Bellman's equation

--- Python 3/First_steps_on_machine_learning/Maze_using_Bellman_equation/Test_Maze.py
+++ Python 3/First_steps_on_machine_learning/Maze_using_Bellman_equation/Test_Maze.py
@@ -1,8 +1,8 @@
 import pygame, sys, maze_builder
 
-class Projection(object):
-    def __init__(self, width, height):
-        super(Projection, self).__init__()
+class Maze(object):
+    def __init__(self, width, height, grid_length): # width and height of the window and the grid size (x, y) so there would be a maximum number of nodes which would be x*y
+        super(Maze, self).__init__()
         pygame.init()
         self.width = width
         self.height = height
@@ -10,42 +10,61 @@ class Projection(object):
         pygame.display.set_caption("TEST MAZE")
         self.background = (0, 0, 0)
         self.nodes = {} # (x, y):maze_builder.Node
-        self.displayNodes = True
-        self.displayEdges = True
-        # self.nodeColour = (0,0,0)
-        # self.edgeColour = (125,125,125)
         self.nodeRadius = 4
+        self.grid_length = grid_length
+        self.node_width = width/grid_length[0]
+        self.node_height = height/grid_length[1]
+
 
     def display(self):
         """ Draw the objects on the screen. """
         self.screen.fill(self.background)
-        for thing in self.objects.values():
-            # if thing.displayEdges:
-            #     for edge in thing.edges:
-            #         pygame.draw.aaline(self.screen, self.edgeColour, (edge.start.x, edge.start.y), (edge.end.x, edge.end.y), 1)
-            for edge in thing.edges:
-                    pygame.draw.aaline(self.screen, thing.edge_color, (edge.start.x, edge.start.y), (edge.end.x, edge.end.y), 1)
-            # if thing.displayNodes:
-            #     for node in thing.nodes:
-            #         pygame.draw.circle(self.screen, self.nodeColour, (int(node.x), int(node.y)), self.nodeRadius, 0)
-            for node in thing.nodes:
-                pygame.draw.circle(self.screen, thing.node_color, (int(node.x), int(node.y)), self.nodeRadius, 0)
+        for position, node in self.nodes.items():
+            # i get the square for the node
+            pygame.draw.rect(self.screen, node.floor_color, (int(node.position[0])*self.node_width, int(node.position[1])*self.node_height, self.node_width, self.node_height), 0) # this is the floor
+            # now lets draw the walls
+            pygame.draw.rect(self.screen, node.wall_color, (int(node.position[0])*self.node_width, int(node.position[1])*self.node_height, self.node_width*0.1, self.node_height*0.1), 0) # top left corner
+            pygame.draw.rect(self.screen, node.wall_color, (int(node.position[0])*self.node_width+self.node_width*0.9, int(node.position[1])*self.node_height, self.node_width*0.1, self.node_height*0.1), 0) # top right corner
+            pygame.draw.rect(self.screen, node.wall_color, (int(node.position[0])*self.node_width+self.node_width*0.9, int(node.position[1])*self.node_height+self.node_height*0.9, self.node_width*0.1, self.node_height*0.1), 0) # bottom right corner
+            pygame.draw.rect(self.screen, node.wall_color, (int(node.position[0])*self.node_width, int(node.position[1])*self.node_height+self.node_height*0.9, self.node_width*0.1, self.node_height*0.1), 0) # bottom left corner
+            # taking wall width equal to 10% of the node size
+            if not node.up: # top wall
+                pygame.draw.rect(self.screen, node.wall_color, (int(node.position[0])*self.node_width+self.node_width*0.09, int(node.position[1])*self.node_height, self.node_width*0.82, self.node_height*0.1), 0)
+            if not node.right: # right wall
+                pygame.draw.rect(self.screen, node.wall_color, (int(node.position[0])*self.node_width+self.node_width*0.9, int(node.position[1])*self.node_height+self.node_height*0.09, self.node_width*0.1, self.node_height*0.82), 0)
+            if not node.down: # bottom wall
+                pygame.draw.rect(self.screen, node.wall_color, (int(node.position[0])*self.node_width+self.node_width*0.09, int(node.position[1])*self.node_height+self.node_height*0.9, self.node_width*0.82, self.node_height*0.1), 0)
+            if not node.left: # left wall
+                pygame.draw.rect(self.screen, node.wall_color, (int(node.position[0])*self.node_width, int(node.position[1])*self.node_height+self.node_height*0.09, self.node_width*0.1, self.node_height*0.82), 0)
+
+        pygame.draw.rect(self.screen, self.runner.color, (self.runner.position[0]*self.node_width+(self.node_width/2-self.runner.width/2), self.runner.position[1]*self.node_height+(self.node_height/2-self.runner.height/2), self.runner.width, self.runner.height), 0)
+
+    def move_runner(self, direction):
+        if getattr(self.nodes[self.runner.position], direction):
+            if direction == "up":
+                self.runner.position = (self.runner.position[0], self.runner.position[1]-1)
+            if direction == "down":
+                self.runner.position = (self.runner.position[0], self.runner.position[1]+1)
+            if direction == "right":
+                self.runner.position = (self.runner.position[0]+1, self.runner.position[1])
+            if direction == "left":
+                self.runner.position = (self.runner.position[0]-1, self.runner.position[1])
 
     def run(self):
         """Create a pygame screen until it is closed."""
         key_to_function = {
-            pygame.K_LEFT:   (lambda x: x.rotateAll('Y', -0.1)),
-            pygame.K_RIGHT:   (lambda x: x.rotateAll('Y', 0.1)),
-            pygame.K_UP:   (lambda x: x.rotateAll('X', -0.1)),
-            pygame.K_DOWN:   (lambda x: x.rotateAll('X', 0.1)),
-            pygame.K_q:   (lambda x: x.rotateAll('Z', -0.1)),
-            pygame.K_e:   (lambda x: x.rotateAll('Z', 0.1)),
-            pygame.K_d:  (lambda x: x.move_all('x',  10)),
-            pygame.K_a:   (lambda x: x.move_all('x', -10)),
-            pygame.K_s:   (lambda x: x.move_all('y',  10)),
-            pygame.K_w:     (lambda x: x.move_all('y', -10)),
-            pygame.K_LSHIFT: (lambda x: x.scaleAll(1.25)),
-            pygame.K_LCTRL:  (lambda x: x.scaleAll( 0.8))
+            pygame.K_LEFT:   (lambda x: x.move_runner("left")),
+            pygame.K_RIGHT:   (lambda x: x.move_runner("right")),
+            pygame.K_UP:   (lambda x: x.move_runner("up")),
+            pygame.K_DOWN:   (lambda x: x.move_runner("down"))
+            # pygame.K_q:   (lambda x: x.rotateAll('Z', -0.1)),
+            # pygame.K_e:   (lambda x: x.rotateAll('Z', 0.1)),
+            # pygame.K_d:  (lambda x: x.move_all('x',  10)),
+            # pygame.K_a:   (lambda x: x.move_all('x', -10)),
+            # pygame.K_s:   (lambda x: x.move_all('y',  10)),
+            # pygame.K_w:     (lambda x: x.move_all('y', -10)),
+            # pygame.K_LSHIFT: (lambda x: x.scaleAll(1.25)),
+            # pygame.K_LCTRL:  (lambda x: x.scaleAll( 0.8))
         }
         done = False
         while not done:
@@ -57,8 +76,6 @@ class Projection(object):
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         done = True
-                    # if event.key in key_to_function.keys():
-                    #     key_to_function[event.key](self)
 
             pygame.time.delay(100)
             self.display()
@@ -66,5 +83,71 @@ class Projection(object):
         pygame.quit()
         return done
 
-    def add_node(self, position, node):
-        self.objects[position] = node
+    def add_node(self, node):
+        type = node.special
+        if type == "start":
+            self.start = node.position
+        if type == "end":
+            self.end = node.position
+        self.nodes[node.position] = node
+
+    def add_runner(self, runner):
+        if runner.width == "relative":
+            runner.width = self.node_width*0.4
+        if runner.height == "relative":
+            runner.height = self.node_height*0.4
+        if runner.position == None:
+            runner.position = self.start
+
+        self.runner = runner
+
+class Maze_runner(object):
+    """docstring for Maze_runner."""
+
+    def __init__(self, position=None, color=(255, 255, 255), width="relative", height="relative"):
+        super(Maze_runner, position).__init__()
+        self.position = position
+        self.color = color
+        self.width = width
+        self.height = height
+
+
+runner = Maze_runner()
+
+# x x
+#   x x
+# x x
+maze = Maze( 1000, 1000, (3, 3))
+test_node_00 = maze_builder.Node(position=(0, 0), right = True, special="start")
+test_node_10 = maze_builder.Node(position=(1, 0), left = True, down = True, right = True)
+test_node_20 = maze_builder.Node(position=(2, 0), down = True, left = True)
+test_node_11 = maze_builder.Node(position=(1, 1), down = True, up = True, right = True, special="bad")
+test_node_21 = maze_builder.Node(position=(2, 1), left = True, up=True, down=True, special="good")
+test_node_22 = maze_builder.Node(position=(2, 2), up = True, left = True)
+test_node_12 = maze_builder.Node(position=(1, 2), up = True, left = True, right = True)
+test_node_02 = maze_builder.Node(position=(0, 2), right = True, special="end")
+maze.add_node(test_node_00)
+maze.add_node(test_node_20)
+maze.add_node(test_node_10)
+maze.add_node(test_node_11)
+maze.add_node(test_node_21)
+maze.add_node(test_node_22)
+maze.add_node(test_node_12)
+maze.add_node(test_node_02)
+maze.add_runner(runner)
+maze.run()
+
+# x x
+# x x
+
+# maze = Maze( 1000, 1000, (2, 2))
+# test_node_00 = maze_builder.Node(position=(0, 0), right = True, up = True, special="start")
+# test_node_10 = maze_builder.Node(position=(1, 0), left = True, down = True)
+# test_node_11 = maze_builder.Node(position=(1, 1), up = True, left = True)
+# test_node_01 = maze_builder.Node(position=(0, 1), right = True, down = True, special="end")
+# maze.add_node(test_node_00)
+# maze.add_node(test_node_10)
+# maze.add_node(test_node_11)
+# maze.add_node(test_node_01)
+# maze.add_runner(runner)
+# maze.run()
--- Python 3/First_steps_on_machine_learning/Maze_using_Bellman_equation/maze_builder.py
+++ Python 3/First_steps_on_machine_learning/Maze_using_Bellman_equation/maze_builder.py
@@ -2,14 +2,30 @@ import pygame
 class Node(object):
     """docstring for Node."""
 
-    def __init__(self, size, up=False, down=False, left=False, right=False, price=0, end=False, floor_color=(0, 0, 0), wall_color=(255, 255, 255), position=None):
+    def __init__(self, size=None, up=False, down=False, left=False, right=False, price=0, special=None, floor_color=(0, 0, 0), wall_color=(255, 255, 255), position=None):
         super(Node, self).__init__()
+        # specials = {
+        #     "start":(lambda x: setattr(x, floor_color, (150, 150, 255))),
+        #     "end":(lambda x: setattr(x, floor_color, (255, 255, 0))),
+        #     "good":(lambda x: setattr(x, floor_color, (150, 255, 150))),
+        #     "bad":(lambda x: setattr(x, floor_color, (255, 150, 150)))
+        # }
+        floor_colors = {
+            None:(0, 0, 0),
+            "start":(150, 150, 255),
+            "end":(255, 255, 0),
+            "good":(0, 255, 0),
+            "bad":(255, 0, 0)
+        }
+        self.special = special
         self.size = size
         self.up = up
         self.down = down
         self.left = left
         self.right = right
-        self.floor_color = floor_color
+        self.floor_color = floor_colors[special]
         self.wall_color = wall_color
+        # if special != None:
+        #     specials[special](self)
         if position != None:
             self.position = position
